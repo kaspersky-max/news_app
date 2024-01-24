df35a2b3819f18993315532089563ce79fcd5df2
Stefan <stefan.arsenic@outlook.com>
2024-01-24 13:27:19 +0100

    login i register funkcije

---
+++ app/src/main/java/com/example/news_app/DBHelper.java
@@ -0,0 +1,52 @@
+package com.example.news_app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "NewsApp.db";
+
+    public DBHelper(Context context) {
+        super(context, name: "NewsApp.db", factory: null, version: 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase mydb) {
+        mydb.execSQL("create Table korisnik(username TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase mydb, int i, int i1) {
+        mydb.execSQL("drop Table if exists korisnik");
+    }
+
+    public Boolean insertData(String username, String password){
+        SQLiteDatabase mydb = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = mydb.insert("korisnik", null, contentValues);
+        if(result==-1) return false;
+        else return true;
+    }
+
+    public Boolean checkUsername(String username){
+        SQLiteDatabase mydb = this.getWritableDatabase();
+        Cursor cursor = mydb.rawQuery("Select * from korisnik where username=?", new String[]{username});
+        if(cursor.getCount()>0) return true;
+        else return false;
+    }
+
+    public Boolean checkUser(String username, String password){
+        SQLiteDatabase mydb = this.getWritableDatabase();
+        Cursor cursor = mydb.rawQuery("Select * from korisnik where username=? and password=?", new String[]{username, password});
+        if(cursor.getCount()>0) return true;
+        else return false;
+    }
+}
---
+++ app/src/main/java/com/example/news_app/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.news_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.usernameLogin);
+        password = (EditText) findViewById(R.id.passwordLogin);
+        btnlogin = (Button) findViewById(R.id.btnloginLogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
